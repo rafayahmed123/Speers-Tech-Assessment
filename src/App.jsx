016c17228ab3ae7d81f346ca60da728047b6ceeb
Siddharth Yadav <siddharthyadav@MacBook-Pro.local>
2021-12-22 10:00:38 -0500

    functionality completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,199 +1,167 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { BiPhoneIncoming } from "react-icons/bi";
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom";
 
-import Header from './Header.jsx';
-import Info from './Info.jsx'
+import Header from "./Header.jsx";
+import Info from "./Info.jsx";
 
 const App = () => {
-  
-  const [ calls, setCalls ] = useState([])
-  const [ archived, setArchived ] = useState([])
-  const [ visible, setVisible ] = useState([])
+  const [calls, setCalls] = useState([]);
+  const [archived, setArchived] = useState([]);
+  const [visible, setVisible] = useState([]);
 
-  const [ showCalls, setShowCalls ] = useState(true)
+  const [showCalls, setShowCalls] = useState(true);
 
   useEffect(() => {
     fetch("https://aircall-job.herokuapp.com/activities")
-      .then(res => res.json())
-      .then((result) =>{
-        setCalls(result)
-        setVisible(result)
-      })
+      .then((res) => res.json())
+      .then((result) => {
+        setCalls(result);
+        setVisible(result);
+      });
   }, []);
 
-
   const archiveCall = (id) => {
-    calls.forEach(call => {
-      if (call.id === id){
+    calls.forEach((call) => {
+      if (call.id === id) {
         call.is_archived = true;
       }
-    })
+    });
 
-    let archivedCalls = []
-    let newCalls = []
-    
-    calls.filter( (call, index) => {
-      if (call.is_archived){
-        console.log("false" + index)
-        archivedCalls.push(call)
+    let archivedCalls = [];
+    let newCalls = [];
+
+    calls.filter((call, index) => {
+      if (call.is_archived) {
+        console.log("false" + index);
+        archivedCalls.push(call);
+      } else {
+        newCalls.push(call);
       }
-      else{
-        newCalls.push(call)
-      }
-    })
+    });
 
-    archived.forEach( call => { archivedCalls.push(call) })
+    archived.forEach((call) => {
+      archivedCalls.push(call);
+    });
 
-    setCalls(newCalls)
-    setArchived(archivedCalls)
+    setCalls(newCalls);
+    setArchived(archivedCalls);
 
-    setVisible(newCalls)
-  }
-  
+    setVisible(newCalls);
+  };
+
   const unArchiveCall = (id) => {
-
-    archived.forEach(call => {      //changing selected archived call property
-      if (call.id === id){
+    archived.forEach((call) => {
+      //changing selected archived call property
+      if (call.id === id) {
         call.is_archived = false;
       }
-    })
+    });
 
-    let archivedCalls = []
-    let newCalls = []
-    
-    archived.filter( (call, index) => {   //setting up updated calls and archived 
-      if (!call.is_archived){
-        newCalls.push(call)
+    let archivedCalls = [];
+    let newCalls = [];
+
+    archived.filter((call, index) => {
+      //setting up updated calls and archived
+      if (!call.is_archived) {
+        newCalls.push(call);
+      } else {
+        archivedCalls.push(call);
       }
-      else{
-        archivedCalls.push(call)
-      }
-    })
+    });
 
-    calls.forEach( call => { newCalls.push(call) }) //adding in any remaining calls
+    calls.forEach((call) => {
+      newCalls.push(call);
+    }); //adding in any remaining calls
 
-    setCalls(newCalls)    
-    setArchived(archivedCalls)
+    setCalls(newCalls);
+    setArchived(archivedCalls);
 
-    setVisible(archivedCalls)
-
-  }
+    setVisible(archivedCalls);
+  };
 
   const archiveAll = () => {
+    let archivedCalls = [];
+    let newCalls = [];
 
-    let archivedCalls = []
-    let newCalls = []
-
-    calls.forEach(call => {      //changing selected archived call property
+    calls.forEach((call) => {
+      //changing selected archived call property
       call.is_archived = true;
-      archivedCalls.push(call)
-    })
+      archivedCalls.push(call);
+    });
 
-    archived.forEach(call => {
-      archivedCalls.push(call)
-    })
+    archived.forEach((call) => {
+      archivedCalls.push(call);
+    });
 
-    setArchived(archivedCalls)
+    setArchived(archivedCalls);
 
-    setCalls(newCalls)
-    setVisible(newCalls)
-  }
+    setCalls(newCalls);
+    setVisible(newCalls);
+  };
 
   const unArchiveAll = () => {
-    let archivedCalls = []
-    let newCalls = []
+    let archivedCalls = [];
+    let newCalls = [];
 
-    archived.forEach(call => {      //changing selected archived call property
+    archived.forEach((call) => {
+      //changing selected archived call property
       call.is_archived = false;
-      newCalls.push(call)
-    })
+      newCalls.push(call);
+    });
 
-    calls.forEach(call => {
-      newCalls.push(call)
-    })
+    calls.forEach((call) => {
+      newCalls.push(call);
+    });
 
-    setCalls(newCalls)
+    setCalls(newCalls);
 
-    setArchived(archivedCalls)
-    setVisible(archivedCalls)
-  }
+    setArchived(archivedCalls);
+    setVisible(archivedCalls);
+  };
 
   const showArchived = () => {
-    setVisible(archived)
-    setShowCalls(false)
-    
-  }
+    setVisible(archived);
+    setShowCalls(false);
+  };
 
   const showInbox = () => {
-    setVisible(calls)
-    setShowCalls(true)
-  }
-
-
-  const mapObject = () => {
-    const mapped = visible.map((call, index)=>{
-
-        return(
-          <div className="call-container" key={index}>
-  
-            <div className="date" style={{fontSize: 11, color: 'gray'}}>
-              {call.created_at.substring(0,10)}
-            </div>
-  
-            <div className='call' key={call.id} onClick={()=>{showCalls ? archiveCall(call.id) : unArchiveCall(call.id) }}>
-  
-              <div style={{marginLeft: 10, marginRight: 10, width: 20,}}>
-                <BiPhoneIncoming style={{ color: 'red', width: 20}}/>
-              </div>
-              
-              <div style={{justifyContent:'center',alignItems:'center', fontWeight: '600', textAlign:'center'}}>
-                
-                {call.from} 
-                <div style={{fontWeight: '500', marginTop: 5, fontSize: 12, color: 'gray'}}>
-                  tried to call on {call.to}
-                </div>
-              </div>
-  
-              <div style={{justifyContent:'center',alignItems:'center', marginRight: 10, color: 'gray', fontSize: 12}}>
-                {call.created_at.substring(11,16)}
-              </div>
-  
-            </div>
-          </div>
-            
-        )
-      
-    })
-
-    return mapped
-  }
+    setVisible(calls);
+    setShowCalls(true);
+  };
+         
 
   return (
-    <div className='container'>
-      <Header archived={showArchived} inbox={showInbox}/>
-      <div className='archive-all-container'>
-        {
-          showCalls
-          ?
-          <div className='archive-all-btn' onClick={()=>{archiveAll()}}>
+    <div className="container">
+      <Header archived={showArchived} inbox={showInbox} />
+      <div className="archive-all-container">
+        {showCalls ? (
+          <div
+            className="archive-all-btn"
+            onClick={() => {
+              archiveAll();
+            }}
+          >
             Archive All Calls
           </div>
-          :
-          <div className='archive-all-btn' onClick={()=>{unArchiveAll()}}>
+        ) : (
+          <div
+            className="archive-all-btn"
+            onClick={() => {
+              unArchiveAll();
+            }}
+          >
             Unarchive All Calls
           </div>
-        }
+        )}
       </div>
       <div className="container-view">
-        {mapObject()}
-        {/* <Info map={mapObject}/> */}
+        <Info visible={visible} calls={calls} showCalls={showCalls} archiveCall={archiveCall} unArchiveCall={unArchiveCall}/>
       </div>
-      
     </div>
   );
 };
 
-ReactDOM.render(<App/>, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById("app"));
 
 export default App;
--- src/Info.jsx
+++ src/Info.jsx
@@ -1,17 +1,72 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { BiPhoneIncoming } from "react-icons/bi";
 
 const Info = (props) => {
 
-const x = props.map()
-return(
-<div style={{display: 'flex', flexDirection: 'column',width: '100%', height: '100%', justifyContent: 'center',alignItems:'center'}}>
-    {x}
-</div>
-    
+  const mapObject = () => {
+    const mapped = props.visible.map((call, index) => {
+        return (
+          <div className="call-container" key={index}>
+            <div className="date" style={{ fontSize: 11, color: "gray" }}>
+              {call.created_at.substring(0, 10)}
+            </div>
+  
+            <div
+              className="call"
+              key={call.id}
+              onClick={() => {
+                props.showCalls ? props.archiveCall(call.id) : props.unArchiveCall(call.id);
+              }}
+            >
+              <div style={{ marginLeft: 10, marginRight: 10, width: 20 }}>
+                <BiPhoneIncoming style={{ color: "red", width: 20 }} />
+              </div>
+  
+              <div
+                style={{
+                  justifyContent: "center",
+                  alignItems: "center",
+                  fontWeight: "600",
+                  textAlign: "center",
+                }}
+              >
+                {call.from}
+                <div
+                  style={{
+                    fontWeight: "500",
+                    marginTop: 5,
+                    fontSize: 12,
+                    color: "gray",
+                  }}
+                >
+                  tried to call on {call.to}
+                </div>
+              </div>
+  
+              <div
+                style={{
+                  justifyContent: "center",
+                  alignItems: "center",
+                  marginRight: 10,
+                  color: "gray",
+                  fontSize: 12,
+                }}
+              >
+                {call.created_at.substring(11, 16)}
+              </div>
+            </div>
+          </div>
+        );
+      });
+      return mapped
+  }
 
-)
-
-}
+  return (
+      <div className='info-container'>
+        {mapObject()}
+      </div>
+   
+  );
+};
 
 export default Info;
